e5930474c3fbc9abbf3e072d29838a22c01ba9e7
joaovitorevora <joaovitorevora@gmail.com>
2023-01-27 16:45:10 -0300

    adicionando efeito de click no card

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,10 @@ $(document).ready(() => {
 	}, 80);
 
 });
+// Contato -----------
+const card = document.querySelector('.card')
+card.addEventListener("click",(e)=>{
+	card.classList.toggle("flip")
+})
+
+
