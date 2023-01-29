d0d81b72d90b86828f43ed305cd7ff178e8db1b1
joaovitorevora <joaovitorevora@gmail.com>
2023-01-29 02:39:53 -0300

    Adicionando API CEP, e adicionando modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,4 +24,44 @@ cards.forEach(card => {
     });
 });
 
+// Lista de espera ----------
+
+document.getElementById("pesquisarCEP").addEventListener("click", pesquisarCEP);
+
+function pesquisarCEP() {
+    // Obtendo o valor do campo de entrada de CEP
+    var cep = document.getElementById("cep").value;
+
+    // Verificando se o CEP é válido
+    if (validarCEP(cep)) {
+        // Fazendo a requisição à API de busca de CEP
+        fetch("https://viacep.com.br/ws/" + cep + "/json/")
+            .then(response => response.json())
+            .then(data => preencherDados(data))
+            .catch(error => {
+                document.getElementById("cepNaoEncontrado").style.display = "block";
+            });
+    } else {
+        document.getElementById("cepInvalido").style.display = "block";
+    }
+}
+
+function validarCEP(cep) {
+    // validar o CEP
+    var validacep = /^[0-9]{8}$/;
+
+    return validacep.test(cep);
+}
+
+function preencherDados(data) {
+    // Preenchendo os campos com os dados retornados pela API
+    document.getElementById("rua").value = data.logradouro;
+    document.getElementById("cidade").value = data.localidade;
+    document.getElementById("estado").value = data.uf;
+}
+	// fecha modal
+document.getElementById("modal-fechar").addEventListener("click", function () {
+    document.getElementById("cepInvalido").style.display = "none";
+});
+
 
